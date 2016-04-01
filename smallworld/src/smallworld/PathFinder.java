9b410442b447028dc2417de7010820550621cf0c
Jluedtke19 <jluedtke19@cornellcollege.edu>
2016-04-01 13:20:55 -0500

    Made interface

--- smallworld/src/smallworld/PathFinder.java
+++ smallworld/src/smallworld/PathFinder.java
@@ -4,21 +4,68 @@ import edu.princeton.cs.In;
 import edu.princeton.cs.StdIn;
 import edu.princeton.cs.StdOut;
 import java.util.Stack;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Runs breadth first search algorithm from source s on a graph G. After
  * preprocessing the graph, can process shortest path queries from s to any
  * vertex t.
  *
- *****************************************************************************
  */
-public class PathFinder {
+public class PathFinder{
 
     // prev[v] = previous vertex on shortest path from s to v
     // dist[v] = length of shortest path from s to v
     private ST<String, String> prev = new ST<String, String>();
     private ST<String, Integer> dist = new ST<String, Integer>();
+    
+    private JPanel p;       //Panel
+    private JFrame f;       //Frame
+    public static String start;    //The start station
+    public static String fin;   //The station going to
 
+    /**
+     * Put in the main method to start the process of making the panel and frame
+     */
+    public PathFinder(){
+        gui();
+    }
+    /**
+     * Creates frame and a panel
+     */
+    public void gui(){
+        f = new JFrame();
+        f.setVisible(true);
+        f.setSize(600,400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        p = new JPanel();
+        p.setBackground(Color.WHITE);
+        
+        f.add(p);
+        
+    }
+    private void TextField(){
+        final JTextField start = new JTextField(6);
+        final JTextField end = new JTextField(6);
+        
+        JButton finalize = new JButton("Start");
+        finalize.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String data = start.getText();
+                String fin = end.getText();
+            }
+        });
+        
+        p.add(start);
+        p.add(end);
+        p.add(finalize);
+        f.setVisible(true);
+        
+    }
     /**
      * Runs BFS in the graph from a vertex by putting the source in a queue string
      * then removes the vertex v from the queue and inserts it's neighbors.
@@ -30,8 +77,8 @@ public class PathFinder {
 
         // put source on the queue
         Queue<String> q = new Queue<String>();
-        q.enqueue(s);
-        dist.put(s, 0);
+        q.enqueue(PathFinder.fin);
+        dist.put(PathFinder.fin, 0);
 
         // repeated remove next vertex v from queue and insert
         // all its neighbors, provided they haven't yet been visited
@@ -81,6 +128,19 @@ public class PathFinder {
         }
         return path;
     }
+    /**
+     * Function will be called in the gui to start the process there.
+     */
+    public static void startProcess(){
+        In in = new In("train.txt");
+        Graph G = GraphGenerator.read(in, " ");
+//        String s = args[2];
+        
+        PathFinder pf = new PathFinder(G, PathFinder.fin);
+        pf.report(pf.start);
+        PathFinder pathFinder = new PathFinder();
+        pathFinder.TextField();
+    }
 
 /**
  * Computes the smallest distance between two places
@@ -90,16 +150,11 @@ public class PathFinder {
         String filename = args[0];
         String delimiter = args[1];
 
-        System.out.println(filename);
-        System.out.println(">" + delimiter + "<");
-        In in = new In(filename);
-        Graph G = GraphGenerator.read(in, delimiter);
-        String s = args[2];
-        PathFinder pf = new PathFinder(G, s);
-
-        pf.report("E30");
-
+        Guismall guismall = new Guismall();
+//        guismall.setPath(this);
+        guismall.setVisible(true);
     } // main( String [] )
+    
 /**
  * Function to find a path to a location and gives the distance
  * @param airport The name of the location
@@ -111,5 +166,5 @@ public class PathFinder {
         }
         StdOut.println("distance " + this.distanceTo(airport));
     } // report( PathFinder, String )
-
+    
 } // PathFinder
